6543d58898ad9923df8887d9ee94a468ed43972a
hmartinez00 <hectoralonzomartinez00@gmail.com>
2022-12-15 15:55:57 -0400

    Version estable 20221215 1555

--- transporte.py
+++ transporte.py
@@ -4,27 +4,11 @@ tabla = input('Ingrese el nombre del archivo: ')
 
 datos_json = \
 {
-    "2022-12-15":
-    {
-        "Ver":"Lucas 4:42-44",
-        "Sub":"Jesús recorre Galilea predicando",
-        "Tex":
-        {
-            "42":"Cuando ya era de día, salió y se fue a un lugar desierto; y la gente le buscaba, y llegando a donde estaba, le detenían para que no se fuera de ellos.",
-            "43":"Pero él les dijo: Es necesario que también a otras ciudades anuncie el evangelio del reino de Dios; porque para esto he sido enviado.",
-            "44":"Y predicaba en las sinagogas de Galilea."
-        }
-    },
     "2022-12-16":
     {
         "Ver":"Lucas 5:1-11",
         "Sub":"La pesca milagrosa",
-        "Tex":
-        {
-            "4":"Cuando terminó de hablar, dijo (Jesús) a Simón: Boga mar adentro, y echad vuestras redes para pescar. ",
-            "5":"Respondiendo Simón, le dijo: Maestro, toda la noche hemos estado trabajando, y *nada hemos pescado; mas en tu palabra echaré la red.*",
-            "6":"Y habiéndolo hecho, encerraron gran cantidad de peces, y su red se rompía."
-        }
+        "Tex":""
     }
 }
 
