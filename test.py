03b16600357a005730fc8e0b008b8b38e81c08b7
hmartinez <hmartinez@abae.gob.ve>
2022-12-17 14:37:08 -0400

    Actualizando sistema de transporte de datos

--- test.py
+++ test.py
@@ -1,24 +1,8 @@
-from ManageDB.sqlite_on_db import *
-import json
-from datetime import datetime as dt
-from General_Utilities.fecha import FechaID
-import sqlite3
+from General_Utilities.option_list import option_list
+import os
 
-# Fecha = FechaID(dt.now())
-Fecha = "2022-12-16"
+files = os.listdir('settings')
 
-database = r"2tim4_1.db"
-table = 'aguas_vivas_pasajes'
+tabla = option_list(files)
 
-campo = 'Id'
-a = '1'
-b = '10'
-
-sql = 'UPDATE ' + table + ' SET ' \
-f'"{campo}"={b} WHERE "{campo}"={a}'
-conn = sqlite3.connect(database)
-# cursor = conn.cursor()
-conn.execute(sql)
-conn.close()
-
-# reset_count(database, table, campo, a, b)
+print(tabla)
--- transporte.py
+++ transporte.py
@@ -1,18 +1,65 @@
 import json
+import os
+from General_Utilities.option_list import option_list
 
-tabla = input('Ingrese el nombre del archivo: ')
+files = os.listdir('settings')
 
-datos_json = \
-{
-    "2022-12-16":
-    {
-        "Ver":"Lucas 5:1-11",
-        "Sub":"La pesca milagrosa",
-        "Tex":""
-    }
-}
+tabla = option_list(files)
 
-ruta_archivo_json = f'settings/{tabla}.json'
+print(tabla)
+
+ruta_archivo_json = f'settings/{tabla}'
+
+
+Fecha = input('Introduzca la fecha: ')
+Ver = input('Introduzca el pasaje: ')
+Sub = input('Introduzca el subtitulo: ')
+
+sub_param = {}
+
+pregunta = 's'
+while pregunta == 's':
+    if pregunta == 's':
+        string = input('Introduzca el texto: ')
+
+        vers = string.split(' ')[0]
+
+        new_string = ''
+        for i in string.split(' ')[1:]:
+            new_string = new_string + i + ' '
+
+        sub_param[vers] = new_string
+
+        pregunta = input('Desea continuar? (s/n) ')
+    else:
+        break
+
+
+param = {}
+param["Ver"] = Ver
+param["Sub"] = Sub
+param["Tex"] = sub_param
+
+# print(param)
+
+
+with open(ruta_archivo_json) as archivo_json:
+    datos_json = json.load(archivo_json)
+
+datos_json[Fecha] = param
+
+# datos_json = \
+# {
+#     "2022-12-17":
+#     {
+#         "Ver":"Lucas 5:1-11",
+#         "Sub":"La pesca milagrosa",
+#         "Tex":{
+#             "8":"Viendo esto Simón Pedro, cayó de rodillas ante Jesús, diciendo: Apártate de mí, Señor, porque soy hombre pecador.",
+#             "9":"Porque por la pesca que habían hecho, el temor se había apoderado de él, y de todos los que estaban con él,"
+#         }
+#     }
+# }
 
 with open(ruta_archivo_json, 'w', encoding='utf8') as archivo_json:
     json.dump(datos_json, archivo_json, indent=4)
