d5a46f82937397584fc96ffd9138b0bd5f874f86
hmartinez00 <hectoralonzomartinez00@gmail.com>
2022-12-30 12:19:48 -0400

    Version Estable 20221230 121948

--- test.py
+++ test.py
@@ -1,11 +1,30 @@
 from General_Utilities.speech_recognizer import Reconocimiento
 
+file = 'tmp.txt'
+
+string = ''
+
+f = open(file, 'w')
+f.write(string)
+f.close()
+
 valor = False
 
 while valor == False:
-    Reconocimiento()
+    dictado = Reconocimiento()
     pregunta = input('\nCoincide con lo dicho? (S/N): ')
     if pregunta == 's' or pregunta == 'S':
+
+        f = open(file, 'r')
+        string = f.read()
+        f.close()
+
+        string = string + str(dictado)
+
+        f = open(file, 'w')
+        f.write(string)
+        f.close()
+
         valor = True
     elif pregunta == 'n' or pregunta == 'N':
         continue
