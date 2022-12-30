5a18c9bcf1080258e64a02cd7e494b7e7fedc50f
hmartinez00 <hectoralonzomartinez00@gmail.com>
2022-12-30 13:18:19 -0400

    Version Estable 20221230 131818

--- test.py
+++ test.py
@@ -7,7 +7,6 @@ if os.path.isfile(file):
     pass
 else:
     string = ''
-
     f = open(file, 'w')
     f.write(string)
     f.close()
@@ -15,21 +14,32 @@ else:
 valor = False
 
 while valor == False:
-    dictado = Reconocimiento()
-    
-    if 'finalizar dictado' in dictado:
-        valor = True
-    
-    f = open(file, 'r')
-    string = f.read()
-    f.close()
-    
-    if 'nueva línea' in dictado:
-        string = string + str(dictado) + '\n'
-    else:
-        string = string + str(dictado)
 
-    f = open(file, 'w')
-    f.write(string)
-    f.close()
+    try:
+        dictado = Reconocimiento()
+        
+        if 'finalizar dictado' in dictado:
+            break
+        
+        f = open(file, 'r')
+        string = f.read()
+        f.close()
+        
+        if 'nueva línea' in dictado:
+            string = string + '\n'
+        elif 'nuevo párrafo' in dictado:
+            string = string + '\n\n'
+        elif 'borrar todo' in dictado:
+            string = ''
+            f = open(file, 'w')
+            f.write(string)
+            f.close()
+        else:
+            string = string + ' ' + str(dictado)
+
+        f = open(file, 'w')
+        f.write(string)
+        f.close()
     
+    except:    
+        continue
