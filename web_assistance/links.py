267a776f6465c6cd962e2d4330ffdac779401c8f
hmartinez00 <hectoralonzomartinez00@gmail.com>
2023-02-24 12:55:34 -0400

    Version Estable 20230224 125532

--- web_assistance/links.py
+++ web_assistance/links.py
@@ -1,5 +1,6 @@
 from bs4 import BeautifulSoup
 import requests
+import webbrowser
 import pandas as pd
 from General_Utilities.control_rutas import setting_routes
 from modules.info_tools import general_estract
@@ -8,11 +9,30 @@ from modules.info_tools import general_estract
 # -------------------------------------
 # Extraemos los datos de la url
 # -------------------------------------
-url = 'https://www.youtube.com/playlist?list=PLuPPHFMyRGuJAEf8OWQHklU-I5q5-SLjq'
+url_list = [
+    "http://www.doinggood.org/BibleQuizzesQFSpSound/Old%20Testament/Genesis%201-11.htm",
+    "http://www.doinggood.org/BibleQuizzesQFSpSound/Old%20Testament/Genesis%2012-27.htm",
+    "http://www.doinggood.org/BibleQuizzesQFSpSound/Old%20Testament/Genesis%2028-37.htm",
+    "http://www.doinggood.org/BibleQuizzesQFSpSound/Old%20Testament/Genesis%2038-50.htm",
+    "http://www.doinggood.org/BibleQuizzesQFSpSound/Old%20Testament/Exodo%201-15.htm",
+    "http://www.doinggood.org/BibleQuizzesQFSpSound/Old%20Testament/Exodo%2016-40.htm",
+    "http://www.doinggood.org/BibleQuizzesQFSpSound/Old%20Testament/Levitico%201-14.htm",
+    "http://www.doinggood.org/BibleQuizzesQFSpSound/Old%20Testament/Levitico%2015-27.htm",
+    "http://www.doinggood.org/BibleQuizzesQFSpSound/Old%20Testament/Numeros%201-11.htm",
+    "http://www.doinggood.org/BibleQuizzesQFSpSound/Old%20Testament/Numeros%2012-21.htm",
+    "http://www.doinggood.org/BibleQuizzesQFSpSound/Old%20Testament/Numeros%2022-36.htm",
+    "http://www.doinggood.org/BibleQuizzesQFSpSound/Old%20Testament/Deuteronomio%201-13.htm",
+    "http://www.doinggood.org/BibleQuizzesQFSpSound/Old%20Testament/Deuteronomio%2014-21.htm",
+    "http://www.doinggood.org/BibleQuizzesQFSpSound/Old%20Testament/Deuteronomio%2022-34.htm"
+]
 
-response    = requests.get(url                          )
-bs          = BeautifulSoup(response.text, 'html.parser')
 
-links = bs.find_all('a')
+for url in url_list:
+    # response    = requests.get(url                          )
+    # bs          = BeautifulSoup(response.text, 'html.parser')
 
-print(links)
+    # links = bs.find('title').get_text()
+
+    # print(links)
+
+    webbrowser.open_new_tab(url)
