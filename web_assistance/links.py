2b0ce869026d9e09461a5bca31bae72609958ee7
hmartinez <hmartinez@abae.gob.ve>
2023-02-26 12:45:13 -0400

    Version Estable 20230226 124504

--- web_assistance/links.py
+++ web_assistance/links.py
@@ -2,6 +2,7 @@ from bs4 import BeautifulSoup
 import requests
 import webbrowser
 import pandas as pd
+import time
 from General_Utilities.control_rutas import setting_routes
 from modules.info_tools import general_estract
 
@@ -10,27 +11,16 @@ from modules.info_tools import general_estract
 # Extraemos los datos de la url
 # -------------------------------------
 url_list = [
-    "http://www.doinggood.org/BibleQuizzesQFSpSound/MajorProphets/Isaias%201-22.htm",
-    "http://www.doinggood.org/BibleQuizzesQFSpSound/MajorProphets/Isaias%2023-44.htm",
-    "http://www.doinggood.org/BibleQuizzesQFSpSound/MajorProphets/Isaias%2045-66.htm",
-    "http://www.doinggood.org/BibleQuizzesQFSpSound/MajorProphets/Jeremias%201-17.htm",
-    "http://www.doinggood.org/BibleQuizzesQFSpSound/MajorProphets/Jeremias%2018-31.htm",
-    "http://www.doinggood.org/BibleQuizzesQFSpSound/MajorProphets/Jeremias%2032-52.htm",
-    "http://www.doinggood.org/BibleQuizzesQFSpSound/MajorProphets/Lamentaciones.htm",
-    "http://www.doinggood.org/BibleQuizzesQFSpSound/MajorProphets/Ezequiel%201-21.htm",
-    "http://www.doinggood.org/BibleQuizzesQFSpSound/MajorProphets/Ezequiel%2022-48.htm",
-    "http://www.doinggood.org/BibleQuizzesQFSpSound/MajorProphets/Daniel.htm",
-    "http://www.doinggood.org/BibleQuizzesQFSpSound/MinorProphets/Oseas.htm",
-    "http://www.doinggood.org/BibleQuizzesQFSpSound/MinorProphets/Joel.htm",
-    "http://www.doinggood.org/BibleQuizzesQFSpSound/MinorProphets/Amos.htm",
-    "http://www.doinggood.org/BibleQuizzesQFSpSound/MinorProphets/Abdias.htm",
-    "http://www.doinggood.org/BibleQuizzesQFSpSound/MinorProphets/Miqueas.htm",
-    "http://www.doinggood.org/BibleQuizzesQFSpSound/MinorProphets/Nahum.htm",
-    "http://www.doinggood.org/BibleQuizzesQFSpSound/MinorProphets/Habacuc.htm",
-    "http://www.doinggood.org/BibleQuizzesQFSpSound/MinorProphets/Sofonias.htm",
-    "http://www.doinggood.org/BibleQuizzesQFSpSound/MinorProphets/Hageo.htm",
-    "http://www.doinggood.org/BibleQuizzesQFSpSound/MinorProphets/Zacarias.htm",
-    "http://www.doinggood.org/BibleQuizzesQFSpSound/MinorProphets/Malaquias.htm"
+    "http://www.doinggood.org/BibleQuizzesQFSpSound/General%20EpistlesQF/Santiago%201-2.htm",
+    "http://www.doinggood.org/BibleQuizzesQFSpSound/General%20EpistlesQF/Santiago%203-5.htm",
+    "http://www.doinggood.org/BibleQuizzesQFSpSound/General%20EpistlesQF/1%20Pedro%201-2.htm",
+    "http://www.doinggood.org/BibleQuizzesQFSpSound/General%20EpistlesQF/1%20Pedro%203-5.htm",
+    "http://www.doinggood.org/BibleQuizzesQFSpSound/General%20EpistlesQF/1%20Juan.htm",
+    "http://www.doinggood.org/BibleQuizzesQFSpSound/General%20EpistlesQF/2%20&amp;%203%20Juan.htm",
+    "http://www.doinggood.org/BibleQuizzesQFSpSound/Apocalipsis%20w%20sd/Apocalipsis%201-5.htm",
+    "http://www.doinggood.org/BibleQuizzesQFSpSound/Apocalipsis%20w%20sd/Apocalipsis%206-11.htm",
+    "http://www.doinggood.org/BibleQuizzesQFSpSound/Apocalipsis%20w%20sd/Apocalipsis%2012-16.htm",
+    "http://www.doinggood.org/BibleQuizzesQFSpSound/Apocalipsis%20w%20sd/Apocalipsis%2017-22.htm"
 ]
 
 
@@ -43,3 +33,4 @@ for url in url_list:
     # print(links)
 
     webbrowser.open_new_tab(url)
+    time.sleep(1)
