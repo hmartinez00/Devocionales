645eeb82dcfa8a945e13a20433b5ea7cbff7a487
hmartinez00 <hectoralonzomartinez00@gmail.com>
2023-03-25 10:21:10 -0400

    Version Estable 20230325 102109

--- msgo_creator.py
+++ msgo_creator.py
@@ -4,7 +4,7 @@ from General_Utilities.fecha import format_FechaID
 from General_Utilities.option_list import option_list
 from General_Utilities.control_rutas import setting_routes
 from MessagesKit.str_msg_format import buildmessage as bm
-from modules.msgo_creator_module import msgo_sender
+from modules.msgo_creator_module import msgo_sender, general_msgo_sender
 
 database = r"2tim4_1.db"
 Fecha = input('Introduzca la fecha: ')
@@ -14,5 +14,14 @@ key = 'tables'
 tables = setting_routes(key)
 option = option_list(tables)
 
-
-msgo_sender(database, option, Fecha)
+if option != 'settings/tables/plan_biblia_52.json':
+    msgo_sender(database, option, Fecha)
+else:
+    table = str(option).split('/')[-1].split('.')[0]
+    List = [
+        'Lectura',
+        'Titulo',
+        'Intro',
+        'Encuesta'
+    ]
+    general_msgo_sender(database, table, Fecha, List)
